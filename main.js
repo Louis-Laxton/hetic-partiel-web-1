f5c1415f07f234f77c69c9982fc404b886250558
louis.laxton@hetic.net <louis.laxton@outlook.fr>
2018-06-14 12:24:48 +0200

    Fixes #1 + ajust Fixes #2

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ document.addEventListener('wheel', function(event) {
   if (event.deltaY > 0) {
     headerContainer.style.marginTop = "-170px";
   } else if (event.deltaY < 0) {
-    headerContainer.style.marginTop = "-52px";
+    headerContainer.style.marginTop = "0px";
+  }
+
+});
+
+var maiNavList = document.querySelector('#mainav ul');
+var burgerMenu = document.querySelector('#burgerMenu');
+
+burgerMenu.addEventListener('click', function() {
+  if (maiNavList.style.display === "none") {
+    maiNavList.style.display = "block";
+  } else {
+    maiNavList.style.display = "none";
   }
 });
